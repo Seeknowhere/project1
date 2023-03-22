2676e2d50a5bbb813c73c162575918936c5a6169
Karol/Shaira <karolshaira@KarolShairas-iMac.local>
2023-03-21 23:20:53 -0500

    Initial commit

---
+++ includes/header-footer/header.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="./index.php">Home</a></li>
+				<li><a href="./works.php">Works</a></li>
+				<li><a href="./company.php">Company</a></li>
+				<li><a href="./news.php">News</a></li>
+				<li><a href="./contact.php">Contact</a></li>
+				<li><a href="./recruit.php">Recruit</a></li>
+				<li><a href="./privacy.php">Privacy</a></li>
+			</ul>
+		</nav>
+	</header>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<script src="assets/js/index.js"></script>
+</html>
